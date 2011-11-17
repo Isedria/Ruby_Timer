c8aec76ccc9b35592ad0ed73433e50d9af754fa3
Simon Sarkissjan <isedria@hp-isedria.(none)>
2011-11-17 15:04:12 +0100

    Timerauswahl vereinfacht, es gibt nur noch eine Eingabe, die man tätigen muss.

--- lib/main.rb
+++ lib/main.rb
@@ -4,29 +4,26 @@ require 'zeit_messer'
 
 zms = []
 
-3.times do |zm_nr|
+33.times do |zm_nr|
   zms[zm_nr] = ZeitMesser.new
 end
 
-zm_nr = 0
+standard_nr = 1
 loop do
   print 'Timer: '
-  eingabe_befehl = gets.chomp
+  eingabe_kombi = gets.chomp
+  eingabe_befehl, zm_nr = eingabe_kombi.split("", 2)
+  zm_nr = (zm_nr=="") ? standard_nr : zm_nr.to_i
 
-  if eingabe_befehl =~ /s|e|d|x|t/i
-    if eingabe_befehl == "t"
-      print "Timer - Nummer :"
-      zm_nr = gets.to_i
-    end
-
-  
+  if eingabe_befehl =~ /[sedx]/i
     case eingabe_befehl
     when "s" then puts zms[zm_nr].start
     when "e" then puts zms[zm_nr].ende
     when "d" then puts zms[zm_nr].dauer
+    when "x" then break
     end
-
-    break if eingabe_befehl == "x"
+  else
+    puts "Ungültige Eingabe!"
   end
 end
 
